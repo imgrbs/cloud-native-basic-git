6aa5e3e9b3e3438e3b4362b30bd7c858aecbad0b
Keerati <keerati.jear@gmail.com>
2018-08-28 14:07:00 +0700

    [ADD] Add Unit Test User Controller

---
+++ src/test/java/th/in/taehub/MyFirstService/controller/UserControllerTest.java
@@ -0,0 +1,62 @@
+package th.in.taehub.MyFirstService.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+
+public class UserControllerTest {
+
+    UserController userController;
+    ArrayList<User> users;
+    ArrayList<User> user;
+
+    @Before
+    public void setUp() {
+        userController = new UserController();
+        users = new ArrayList<User>();
+
+        for (int i = 0; i < 5; i++) {
+            users.add(new User(1, "Keerati"));
+        }
+
+        user = new ArrayList<User>();
+        user.add(new User(1, "Keerati"));
+    }
+
+    @Test
+    public void getUsersShouldReturnFivePerson() {
+        Assert.assertEquals(5, userController.getUsers().size());
+    }
+
+    @Test
+    public void getUsersShouldReturnIdOneInAllArrayData() {
+        ArrayList<User> controllerUsers = userController.getUsers();
+        Assert.assertEquals(users.get(0).getId(), controllerUsers.get(0).getId());
+        Assert.assertEquals(users.get(0).getName(), controllerUsers.get(0).getName());
+        Assert.assertEquals(users.get(1).getId(), controllerUsers.get(1).getId());
+        Assert.assertEquals(users.get(1).getName(), controllerUsers.get(1).getName());
+        Assert.assertEquals(users.get(2).getId(), controllerUsers.get(2).getId());
+        Assert.assertEquals(users.get(2).getName(), controllerUsers.get(2).getName());
+        Assert.assertEquals(users.get(3).getId(), controllerUsers.get(3).getId());
+        Assert.assertEquals(users.get(3).getName(), controllerUsers.get(3).getName());
+        Assert.assertEquals(users.get(4).getId(), controllerUsers.get(4).getId());
+        Assert.assertEquals(users.get(4).getName(), controllerUsers.get(4).getName());
+    }
+
+
+    @Test
+    public void getUserShouldReturnOneUser() {
+        Assert.assertEquals(1, userController.getUser().size());
+
+    }
+
+
+    @Test
+    public void getUserShouldReturnIdOne() {
+        ArrayList<User> controllerUsers = userController.getUser();
+        Assert.assertEquals(user.get(0).getId(), controllerUsers.get(0).getId());
+        Assert.assertEquals(user.get(0).getName(), controllerUsers.get(0).getName());
+    }
+}
