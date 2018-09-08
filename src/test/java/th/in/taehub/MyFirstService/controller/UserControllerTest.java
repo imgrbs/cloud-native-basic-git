bc922981aad3221a364996bffd56b508a5f0b87d
Keerati <keerati.jear@gmail.com>
2018-09-08 13:33:42 +0700

    [UPDATE] Update testdata

--- src/test/java/th/in/taehub/MyFirstService/controller/UserControllerTest.java
+++ src/test/java/th/in/taehub/MyFirstService/controller/UserControllerTest.java
@@ -24,21 +24,21 @@ public class UserControllerTest {
     public void getUsersShouldReturnIdAndNameCorrectlyInAllArrayData() {
         ArrayList<User> controllerUsers = userController.getUsers();
         Assert.assertEquals(1, controllerUsers.get(0).getId());
-        Assert.assertEquals("Keerati_1", controllerUsers.get(0).getName());
+        Assert.assertEquals("Keerati", controllerUsers.get(0).getName());
         Assert.assertEquals(2, controllerUsers.get(1).getId());
-        Assert.assertEquals("Keerati_2", controllerUsers.get(1).getName());
+        Assert.assertEquals("Putchamon", controllerUsers.get(1).getName());
         Assert.assertEquals(3, controllerUsers.get(2).getId());
-        Assert.assertEquals("Keerati_3", controllerUsers.get(2).getName());
+        Assert.assertEquals("Nathawat", controllerUsers.get(2).getName());
         Assert.assertEquals(4, controllerUsers.get(3).getId());
-        Assert.assertEquals("Keerati_4", controllerUsers.get(3).getName());
+        Assert.assertEquals("Kunchai", controllerUsers.get(3).getName());
         Assert.assertEquals(5, controllerUsers.get(4).getId());
-        Assert.assertEquals("Keerati_5", controllerUsers.get(4).getName());
+        Assert.assertEquals("Kanisorn", controllerUsers.get(4).getName());
     }
 
     @Test
     public void getUserShouldReturnIdAndNameCorrectly() {
         User user = userController.getUser(0);
         Assert.assertEquals(1, user.getId());
-        Assert.assertEquals("Keerati_1", user.getName());
+        Assert.assertEquals("Keerati", user.getName());
     }
 }
